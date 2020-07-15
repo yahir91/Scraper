c6f478407951e7252440f2739b54e0c2af60ec72
Yahir Cardona <yahir.cardona@gmail.com>
2020-07-15 14:57:22 -0600

    tests with rspec added

--- bin/main.rb
+++ bin/main.rb
@@ -1,19 +1,16 @@
-# frozen_string_literal: true
-
 require 'byebug'
 require 'open-uri'
 require 'nokogiri'
-require_relative '../lib/logic.rb'
+require_relative '../lib/article_processor.rb'
 
 puts 'Type the articles you want to search'
 search = gets.chomp
 
-articles = Articles.new(search)
+articles = ArticlesProcessor.new(search)
 
-articles.three_pages
-puts "There are #{articles.article_information.count}, Which one do you want to see?"
+articles.articles_info
+puts "There are #{articles.article_information.count} articles, Which one do you want to see?"
 
 article_index = articles.check_article(gets.chomp.to_i)
-
 
 puts articles.article_information[article_index - 1].to_a
