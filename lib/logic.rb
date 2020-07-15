1a27f1ef8450b490fc86bc5db52955b9ac4efd61
Yahir Cardona <yahir.cardona@gmail.com>
2020-07-15 06:38:10 -0600

    logic created

--- lib/logic.rb
+++ lib/logic.rb
@@ -5,34 +5,27 @@ require 'open-uri'
 require 'nokogiri'
 
 class Articles
-  attr_accessor :unparsed_page, :url, :parsed_page, :number_pages, :link, :all_links, :title, :article_information
-  def initialize(search = 'blood', page = '1')
-    @url = "https://search.scielo.org/?q=sangre&lang=en&count=15&from=1&output=site&sort=&format=summary&fb=&page=1&filter%5Bla%5D%5B%5D=en&q=#{search}&lang=en&page=#{page}"
+  attr_accessor :unparsed_page, :url, :parsed_page, :number_pages, :link, :all_links, :title, :article_information, :page
+  def initialize(search = 'blood', number_pages = 3)
     @page = 1
+    @url = "https://search.scielo.org/?q=sangre&lang=en&count=15&from=#{@from}&output=site&sort=&format=summary&fb=&page=#{@page}&filter%5Bla%5D%5B%5D=en&q=#{search}&lang=en"
+    @search = search
+    @from = 1
     @unparsed_page = URI.open(@url)
     @parsed_page = Nokogiri::HTML(@unparsed_page)
     @title = @parsed_page.css('div.item')[0].css('div.line.source').css('span')[2].children.text
-    @number_pages = @parsed_page.css('div.col-md-6.notTablet.right')[0].text.split(' ')[2]
-    @all_links = []
-    @count = 0
-    @link = @parsed_page.css('div.item')[@count].css('div.col-md-11.col-sm-10.col-xs-11')[0].css('div.line').css('a')[0].attributes['href'].value
     @article_information = []
+    @number_pages = number_pages
   end
 
-  def getting_links
-    while @count <= 15
-      @all_links << @link
-      @count += 1
-    end
-    @all_links
+  def url_change
+    @page += 1
+    @from += 15
+    @url = "https://search.scielo.org/?q=sangre&lang=en&count=15&from=#{@from}&output=site&sort=&format=summary&fb=&page=#{@page}&filter%5Bla%5D%5B%5D=en&q=#{@search}&lang=en"
   end
 
-  def authors
-    authors = []
-  end
-
-  def articles_hash
-    @parsed_page.css('div.item').each do |x|
+  def articles_hash(parsed_page)
+    parsed_page.css('div.item').each do |x|
       article = {
         title: x.css('strong').text,
         authors: x.css('div.line.authors')[0].text.gsub(/\s+/, '').split(';'),
@@ -41,13 +34,25 @@ class Articles
       }
       @article_information << article
     end
+  end
+
+  def three_pages
+    while @page <= @number_pages
+      unparsed_page = URI.open(@url)
+      @parsed_page = Nokogiri::HTML(unparsed_page)
+      articles_hash(@parsed_page)
+      url_change()
+      puts @url
+    end
     @article_information
   end
 end
 
 articles = Articles.new
-articles.articles_hash
+articles.three_pages
+
 result = articles.title
-other = articles.article_information[0][:time]
-p result
-p other
+other = articles.article_information[40]
+puts articles.page
+puts other
+puts articles.url
