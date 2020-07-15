9e035e65669c11b290815b694c9908d0efbb7573
Yahir Cardona <yahir.cardona@gmail.com>
2020-07-15 15:23:40 -0600

    rubocop offenses fixed

--- spec/article_processor_spec.rb
+++ spec/article_processor_spec.rb
@@ -1,27 +1,27 @@
 require_relative '../lib/article_processor.rb'
 
 describe ArticlesProcessor do
-    context 'url_change' do
-        it 'change the url' do
-            article = ArticlesProcessor.new('heart')
-            article.url_change
-            expect(article.page).to eq 2
-        end
+  context 'url_change' do
+    it 'change the url' do
+      article = ArticlesProcessor.new('heart')
+      article.url_change
+      expect(article.page).to eq 2
     end
+  end
 
-    context 'articles_hash' do
-        it 'return an array' do
-            article = ArticlesProcessor.new('heart')
-            article.articles_hash(article.parsed_page)
-            expect(article.article_information[0]).to be_a(Hash)
-         end
+  context 'articles_hash' do
+    it 'return an array' do
+      article = ArticlesProcessor.new('heart')
+      article.articles_hash(article.parsed_page)
+      expect(article.article_information[0]).to be_a(Hash)
     end
+  end
 
-    context 'articles_info' do
-        it 'return 45 arrays' do
-            article = ArticlesProcessor.new('heart')
-            article.articles_info
-            expect(article.article_information.length.to_i).to eq 45
-        end
+  context 'articles_info' do
+    it 'return 45 arrays' do
+      article = ArticlesProcessor.new('heart')
+      article.articles_info
+      expect(article.article_information.length.to_i).to eq 45
     end
+  end
 end
