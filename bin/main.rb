c7177b8cb45d26430694ab0024b728855fc2c698
Yahir Cardona <yahir.cardona@gmail.com>
2020-07-15 13:54:22 -0600

    main project finished

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
-articles = Articles.new(gets.chomp)
+require 'byebug'
+require 'open-uri'
+require 'nokogiri'
+require_relative '../lib/logic.rb'
 
+puts 'Type the articles you want to search'
+search = gets.chomp
+
+articles = Articles.new(search)
+
+articles.three_pages
+puts "There are #{articles.article_information.count}, Which one do you want to see?"
+
+article_index = articles.check_article(gets.chomp.to_i)
+
+
+puts articles.article_information[article_index - 1].to_a
--- lib/logic.rb
+++ lib/logic.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 
 class Articles
   attr_accessor :unparsed_page, :url, :parsed_page, :number_pages, :link, :all_links, :title, :article_information, :page
-  def initialize(search = 'blood', number_pages = 3)
+  def initialize(search = '')
     @page = 1
     @url = "https://search.scielo.org/?q=sangre&lang=en&count=15&from=#{@from}&output=site&sort=&format=summary&fb=&page=#{@page}&filter%5Bla%5D%5B%5D=en&q=#{search}&lang=en"
     @search = search
@@ -15,7 +15,7 @@ class Articles
     @parsed_page = Nokogiri::HTML(@unparsed_page)
     @title = @parsed_page.css('div.item')[0].css('div.line.source').css('span')[2].children.text
     @article_information = []
-    @number_pages = number_pages
+    @number_pages = 3
   end
 
   def url_change
@@ -27,10 +27,10 @@ class Articles
   def articles_hash(parsed_page)
     parsed_page.css('div.item').each do |x|
       article = {
-        title: x.css('strong').text,
-        authors: x.css('div.line.authors')[0].text.gsub(/\s+/, '').split(';'),
-        time: x.css('div.line.source').css('span')[2].children.text + x.css('div.line.source').css('span')[1].children.text,
-        link: x.css('div.col-md-11.col-sm-10.col-xs-11')[0].css('div.line').css('a')[0].attributes['href'].value
+        :title= => x.css('strong').text,
+        :authors= => x.css('div.line.authors')[0].text.gsub(/\s+/, '').split(';'),
+        :time= => x.css('div.line.source').css('span')[2].children.text + x.css('div.line.source').css('span')[1].children.text,
+        :link= => x.css('div.col-md-11.col-sm-10.col-xs-11')[0].css('div.line').css('a')[0].attributes['href'].value
       }
       @article_information << article
     end
@@ -41,18 +41,17 @@ class Articles
       unparsed_page = URI.open(@url)
       @parsed_page = Nokogiri::HTML(unparsed_page)
       articles_hash(@parsed_page)
-      url_change()
-      puts @url
+      url_change
+      puts 'chill'
     end
     @article_information
   end
+
+  def check_article(article_index)
+    until article_index.positive? && article_index <= 45
+      puts 'select a number between 1 and 45'
+      article_index = gets.chomp.to_i
+    end
+    article_index
+  end
 end
-
-articles = Articles.new
-articles.three_pages
-
-result = articles.title
-other = articles.article_information[40]
-puts articles.page
-puts other
-puts articles.url
